2eccbb83058816319feb8f3b4f2cd5afd81abdd5
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2022-03-28 20:43:37 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useEffect } from 'react';
+// import 'cloudinary-video-player/dist/cld-video-player.min.css';
+import MyPlayer from './components/VideoPlayer';
+export default function App() {
+  useEffect(() => {
+    MyPlayer();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Tracking user activities on a cloudinary video</h2>
+      <div className="vid-container">
+        <video
+          controls
+          muted
+          className="cld-video-player cld-fluid"
+          id="player"
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,12 @@
+import { Cloudinary } from 'cloudinary-core';
+const cld = new Cloudinary({ cloud_name: 'ifeomaimoh', secure: true });
+
+function MyPlayer() {
+  const player = cld.videoPlayer('player', {
+    bigPlatButton: 'true',
+    controls: 'true',
+  });
+  player.source('8ba64b04-f0e6-43af-a6a0-d7815646332b_jdh3ty');
+  return;
+}
+export default MyPlayer;
